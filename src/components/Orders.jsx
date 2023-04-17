5c3c5c8dfab52d4dee969e2dcb941c588c319bdc
seo-png <iseohyeon128@gmail.com>
2023-04-17 23:43:11 +0900

    desc 수정

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,7 +1,7 @@
 export default function Orders() {
     return <aside>
         <div className="empty">
-            <div className="title">You don't have any orders</div>
+            <div className="title">You don't have any orders 상품을 선택해 주세요.</div>
             <div className="subtitle">Click on a + to add an order</div>
         </div>
     </aside>;
