816c5efbed9c1dca336dc8805878709c3ffffe5e
seo-png <iseohyeon128@gmail.com>
2023-04-20 08:42:14 +0900

    x btn UI 생성 & 주문내역 흰배경 추가

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,9 +1,11 @@
+import useActions from "../hooks/useActions"
 import useOrders from "../hooks/useOrders"
 import usePrototypes from "../hooks/usePrototypes"
 
 export default function Orders() {
   const orders = useOrders();
   const prototypes = usePrototypes();
+  const {remove} = useActions();
 
   // 초기화면 && 담기 갯수가 0인 경우
   if (orders.length === 0) {
@@ -20,27 +22,35 @@ export default function Orders() {
   // 주문이 1개 이상 담긴 경우
   return (
     <aside>
-       <div className="body">
-         {orders.map(order => {
-           const {id} = order;
-           const prototype = prototypes.find(p => p.id === id);
-           return (
-           <div className="item" key={id}>
-             <div className="img">
-               <video src={prototype.thumbnail} />
-             </div>
-             <div className="content">
-               <p className="title">
-                 {prototype.title} x {order.quantity}
-               </p>
-             </div>
-             <div className="action">
-               <p className="price">
-                 $ {prototype.price * order.quantity}
-               </p>
-             </div>
-           </div>);
-         })}
+       <div className="order">
+        <div className="body">
+          {orders.map(order => {
+            const {id} = order;
+            const prototype = prototypes.find(p => p.id === id);
+            const click = () => {
+              remove(id);
+            }
+            return (
+            <div className="item" key={id}>
+              <div className="img">
+                <video src={prototype.thumbnail} />
+              </div>
+              <div className="content">
+                <p className="title">
+                  {prototype.title} x {order.quantity}
+                </p>
+              </div>
+              <div className="action">
+                <p className="price">
+                  $ {prototype.price * order.quantity}
+                </p>
+                <button className="btn btn--link" onClick={click}>
+                  <i className="icon icon--cross"/>
+                </button>
+              </div>
+            </div>);
+          })}
+        </div>
        </div>
     </aside>
   );
