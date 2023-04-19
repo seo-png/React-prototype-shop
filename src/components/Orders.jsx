9036b34bc151cb2e8ae9b292cdbe608da9a1dd6f
seo-png <iseohyeon128@gmail.com>
2023-04-20 08:36:25 +0900

    주문 담기 UI

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,11 +1,47 @@
 import useOrders from "../hooks/useOrders"
+import usePrototypes from "../hooks/usePrototypes"
 
 export default function Orders() {
-    const orders = useOrders();
-    return <aside>
+  const orders = useOrders();
+  const prototypes = usePrototypes();
+
+  // 초기화면 && 담기 갯수가 0인 경우
+  if (orders.length === 0) {
+    return (
+      <aside>
         <div className="empty">
-            <div className="title">You don't have any orders</div>
-            <div className="subtitle">Click on a + to add an order</div>
+          <div className="title">You don't have any orders</div>
+          <div className="subtitle">Click on a + to add an order</div>
         </div>
-    </aside>;
+      </aside>
+    );
+  }
+  
+  // 주문이 1개 이상 담긴 경우
+  return (
+    <aside>
+       <div className="body">
+         {orders.map(order => {
+           const {id} = order;
+           const prototype = prototypes.find(p => p.id === id);
+           return (
+           <div className="item" key={id}>
+             <div className="img">
+               <video src={prototype.thumbnail} />
+             </div>
+             <div className="content">
+               <p className="title">
+                 {prototype.title} x {order.quantity}
+               </p>
+             </div>
+             <div className="action">
+               <p className="price">
+                 $ {prototype.price * order.quantity}
+               </p>
+             </div>
+           </div>);
+         })}
+       </div>
+    </aside>
+  );
 }
