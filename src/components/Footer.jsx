8c0cd2167d67fb21afb9987ca0aa180dd860e435
leeseohyeonn <seohyeon.lee@domfam.com>
2023-08-10 21:30:29 +0900

    footer: name 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
-    return (
-        <footer>
-        <p>© 2023 Seohyun Lee. All rights reserved.</p>
-      </footer>
-    );
+  return (
+    <footer>
+      <p>© 2023 SeoHyun Lee. All rights reserved.</p>
+    </footer>
+  );
 }
